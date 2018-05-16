b577d4bde22839d84431f31f2c53d146d7447b05
ARTEM Burkatsiy <burtart@ukr.net>
2018-05-16 20:26:13 +0300

    prefinal

--- js/background-video.js
+++ js/background-video.js
@@ -4,7 +4,7 @@
     		"width": 1280,
     		"height": 720,
     		"path": "video1/",
-    		"filename": "blood",
+    		"filename": "BgVideo",
     		"types": ["mp4"],
     		"preload": true,
     		"autoplay": true,
